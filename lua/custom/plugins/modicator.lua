5039ae4f9ff14cf6c32b3b255a3822a0b75256b5
Bitteneite <5614646+Bitteneite@users.noreply.github.com>
2024-03-08 09:48:39 +0400

    Create modicator.lua

---
+++ lua/custom/plugins/modicator.lua
@@ -0,0 +1,25 @@
+-- Modicator: Change relative number color
+return {
+
+	-- GitHub URL
+	'mawkler/modicator.nvim',
+	
+	-- Start + Config
+	config = function()
+		require("modicator").setup({
+			triggers_blacklist = {
+                n = { "d", "y" }
+			}
+		})
+	end,
+		
+	opts = {
+		-- Warn if any required option above is missing. May emit false positives
+		-- if some other plugin modifies them, which in that case you can just
+		-- ignore. Feel free to remove this line after you've gotten Modicator to
+		-- work properly.
+		show_warnings = true,
+	}
+	
+}
+
