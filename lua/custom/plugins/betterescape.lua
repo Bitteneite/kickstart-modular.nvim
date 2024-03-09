ab8f0a130ac5a16b59b5d4a4fbe845c18a9cc949
Bitteneite <5614646+Bitteneite@users.noreply.github.com>
2024-03-09 13:07:40 +0400

    Added "kj" as an escape bind

--- lua/custom/plugins/betterescape.lua
+++ lua/custom/plugins/betterescape.lua
@@ -7,7 +7,7 @@ return {
 	-- Start + Config
 	config = function()
 		require("better_escape").setup({
-			mapping = {"jk"}, -- Keybind
+			mapping = {"jk", "kj"}, -- Keybind
 			timeout = vim.o.timeoutlen, -- the time in which the keys must be hit in ms. Use option timeoutlen by default
 			clear_empty_lines = false, -- clear line after escaping if there is only whitespace
 			keys = "<Esc>",
