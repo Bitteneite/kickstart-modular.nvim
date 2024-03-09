461dc5a7faa68d714a1b89380c5990b304b80ccc
Bitteneite <5614646+Bitteneite@users.noreply.github.com>
2024-03-09 13:08:23 +0400

    Changed Theme

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -1,20 +1,19 @@
--- Set lualine as statusline | See `:help lualine.txt`
+-- Set lualine as statusline
 return {
 
-	-- GitHub URL
-	'nvim-lualine/lualine.nvim',
-	dependencies = {
-		"nvim-tree/nvim-web-devicons"
-	},
-	
-	-- Start + Config
-	config = function()
-		require('lualine').setup {
-			options = {
-				icons_enabled = true,
-				theme = 'gruvbox_dark',
-			},
-		}
-	end,
-	
+  -- GitHub URL
+  'nvim-lualine/lualine.nvim',
+  dependencies = {
+    'nvim-tree/nvim-web-devicons',
+  },
+
+  -- Start + Config
+  config = function()
+    require('lualine').setup {
+      options = {
+        icons_enabled = true,
+        theme = 'powerline',
+      },
+    }
+  end,
 }
