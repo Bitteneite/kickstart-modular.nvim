d2ae2dc0f60c5bd077785c5afb5041c95e8797ea
Bitteneite <5614646+Bitteneite@users.noreply.github.com>
2024-04-24 00:52:46 +0400

    Disabled mouse

--- lua/options.lua
+++ lua/options.lua
@@ -15,7 +15,7 @@ vim.opt.relativenumber = true
 vim.opt.backspace = ''
 
 -- Enable mouse mode, can be useful for resizing splits for example!
-vim.opt.mouse = 'a'
+vim.opt.mouse = 'r'
 
 -- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false
